3c97a7f17933a3f7c946693d886d25a4ea15270b
Kuso0taku <denustuzan@gmail.com>
2025-11-09 00:39:17 +0700

    getl -> geti

--- proc_arch/main.c
+++ proc_arch/main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include <ctype.h> // isdigit
+#include <math.h> // pow
 
-void getl(char*s) {
-  char ch;
+void geti(int*num) {
+  char s[21]; // max number of digits + 1 for int 64-bit
   char *p = s;
-  while ((ch=getchar()) != ' ' && ch!='\n' && ch!='\t' && ch!=EOF) *p++ = ch;
+  char ch;
+  int count=0; // counts numbers in input
+  // reads only numbers
+  while ((ch=getchar()) != ' ' && 
+    ch!='\n' && ch!='\t' && ch!=EOF && isdigit(ch)) {*p++ = ch; count ++;}
+  
+  p = s; // point at start of input string
+  // convert chars to number
+  for (int i=pow(10, count-1); i>0; i/=10, p++) *num += (*p-'0')*i;
 }
 
 int main() {
-  char s[255];
-  getl(s);
-  puts(s);
+  int n;
+  geti(&n);
+  printf("\n%d\n", n);
 }
