78c94d3db5d659a932ea0583ec9ec14fed9a07b6
Kuso0taku <denustuzan@gmail.com>
2025-12-14 18:37:11 +0700

    example of input

---
+++ PyC_converter/in.py
@@ -0,0 +1,13 @@
+x = 42
+y = 0
+if x > 0: y = 1
+if x > 10: print(x)
+while y < 10:
+    y = y+1
+print(y)
+for i in range(10):
+    print(i)
+print("hello")
+x = int(input())
+for i in range(3): print(i)
+print(x)
